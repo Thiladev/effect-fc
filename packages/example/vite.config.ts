59298e7074450b7aad79aba10d954c4a8da07031
Julien Valverdé <julien.valverde@mailo.com>
2025-10-03 18:19:23 +0200

    Fix project config

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -1,4 +1,4 @@
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import path from "node:path"
 import { defineConfig } from "vite"
@@ -7,7 +7,10 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        TanStackRouterVite(),
+        tanstackRouter({
+            target: "react",
+            autoCodeSplitting: true,
+        }),
         react(),
     ],
 
